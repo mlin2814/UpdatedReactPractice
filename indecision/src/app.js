a4648af45986342283337cf82c338e5bcb9e365d
Matt@DESKTOP-M4DJU9O <mlin2814@gmail.com>
2019-07-22 22:45:26 -0400

    Late night additions

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,12 +1,34 @@
 console.log("App is running");
 
 var template = (
-
+  <div>
+    <h1>Indeicision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ol>
+  </div>
 );
+
+var user = {
+	name: 'Matt',
+	age: 24,
+	location: 'NYC'
+};
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>{user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 
 
 // const templateObject = {
