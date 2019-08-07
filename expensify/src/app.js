d6de3bd061a200c5abc1488da84a6c9e52785cee
mjl290 <mjl290@njaes.rutgers.edu>
2019-08-07 17:17:03 -0400

    Further work on routing

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
+const ExpenseDashboardPage = () => (
+  <div>This from my dashboard</div>
+);
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+const AddExpensePage = () => (
+  <div>This from my add expense page</div>
+);
+
+const EditExpensePage = () => (
+  <div>This from my Edit expense page</div>
+);
+
+const HelpPage = () => (
+  <div>This from the Help page</div>
+);
+
+const NotFoundPage = () => (
+  <div>404! - <Link to="/">Go Home</Link></div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Go Create</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Go Edit</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Get Help</NavLink>
+  </header>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'))
